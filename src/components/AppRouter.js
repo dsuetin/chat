3a534f627d8135f892a4898573eb95f73019ab38
dsuetin <suetindaniil@gmail.com>
2023-06-07 23:03:03 +0300

    auth part

---
+++ src/components/AppRouter.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { privatRoutes, publicRoutes } from '../routes';
+import { LOGIN_ROUTE, CHAT_ROUTE } from '../utils/consts';
+const AppRouter = () => {
+    const user = true;
+    console.log("user",user, CHAT_ROUTE, LOGIN_ROUTE);
+    return user ? (
+        <Routes>
+          { 
+            privatRoutes.map(({ path: p, Component: Cp }) => {
+            return <Route key={p} path={p} element={<Cp />} exact />;
+          })}
+          <Route path="*" element={<Navigate to={CHAT_ROUTE} />} />
+        </Routes>
+      )
+      :
+      (
+        <Routes>
+        { 
+          publicRoutes.map(({ path: p, Component: Cp }) => {
+          return <Route key={p} path={p} element={<Cp />} exact />;
+        })}
+        <Route path="*" element={<Navigate to={LOGIN_ROUTE} />} />
+      </Routes>
+      )
+    // return user ? 
+    // (
+    //     <Routes>
+    //         {privatRoutes.map(({path, Component}) => 
+    //             <Route key={path} path={path} component={Component} exact={true}/>
+    //         )}
+    //         <Route to={CHAT_ROUTE}/>
+    //     </Routes>
+
+    // )
+    // :
+    // (
+    //     <Routes>
+    //         {publicRoutes.map(({path, Component}) => 
+    //             <Route key={path} path={path} component={Component} exact={true}/>
+    //         )}
+    //         <Route to={LOGIN_ROUTE}/>
+    //     </Routes>
+    // )
+};
+
+export default AppRouter;
