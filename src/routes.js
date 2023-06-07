3c1ad1a24ab8228023e3fdc9ee5239cda2790918
dsuetin <suetindaniil@gmail.com>
2023-06-07 21:48:18 +0300

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { LOGIN_ROUTE, CHAT_ROUTE } from "./utils/consts";
+import Login from "./components/Login";
+import Chat from "./components/Chat";
+export const publicRoutes = [
+    {
+        path: LOGIN_ROUTE,
+        Component: Login
+    }
+]
+
+export const privatRoutes = [
+    {
+        path: CHAT_ROUTE,
+        Component: Chat
+    }
+]
