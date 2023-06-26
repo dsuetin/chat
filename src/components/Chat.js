d05f5189160ff14930d74e213922c010e72fdc02
dsuetin <suetindaniil@gmail.com>
2023-06-26 23:16:55 +0300

    ad

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from '../index';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Button, Container, Grid, TextField } from '@mui/material';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import Loader from './Loader';
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const Chat = () => {
     console.log('chachachat')
     const { auth, firestore } =useContext(Context);
     const [user] = useAuthState(auth);
+    const [value, setValue] = useState('');
+    const [message, loading] = useCollectionData(firestore.collection('messages').orderBy('createdAt'));
+
+    const sendMessage = async () => {
+        console.log(value)
+        firestore.collection('messages').add({
+            uid: user.uid,
+            displayName: user.displayName,
+            photoUrl: user.photoUrl,
+            text: value,
+            cratedAt: firebase.firestore.FieldValue.serverTimestamp()
+        });
+        setValue('');
+    }
+    if (loading) {
+        return (<Loader/>)
+    }
     return (
         <Container style={{background: 'lightblue'}}>
             <Grid container
@@ -26,9 +48,16 @@ const Chat = () => {
                     >
                         <TextField 
                           fullWidth
+                          rowmax={2}
                           variant={"outlined"}
+                          value={value}
+                          onChange={e => setValue(e.target.value)}
                         />
-                        <Button> Send </Button>
+                        <Button 
+                          variant={'outlined'}
+                          onClick={sendMessage}
+                        > Send </Button>
+                        {/* <Button> Send </Button> */}
                     </Grid>
               </Grid>
             {/* chat */}
